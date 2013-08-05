0ae39ba8a8961fe64ddcc968c1c600316a92f927
oec2003 <oec2003@gmail.com>
2013-08-05 11:10:16 +0800

    修改gitignore，添加代码库管理页面

---
+++ AdminPortal/CodeLibManage/CodeLibList.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="CodeLibList.aspx.cs" Inherits="FW.WT.AdminPortal.CodeLibManage.CodeLibList" %>
+
+<%@ MasterType VirtualPath="~/Main.Master" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderMain" runat="server">
+</asp:Content>
---
+++ AdminPortal/CodeLibManage/CodeLibList.aspx.designer.cs
@@ -0,0 +1,27 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace FW.WT.AdminPortal.CodeLibManage {
+    
+    
+    public partial class CodeLibList {
+        
+        /// <summary>
+        /// Master 属性。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的属性。
+        /// </remarks>
+        public new FW.WT.AdminPortal.Main Master {
+            get {
+                return ((FW.WT.AdminPortal.Main)(base.Master));
+            }
+        }
+    }
+}
